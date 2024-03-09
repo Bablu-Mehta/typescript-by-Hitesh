064354254f38a089205c47b72f1d3be955880e19
Bablu Mehta <babloomehta895012@gmail.com>
2024-03-09 17:08:52 +0530

    a better way to write functions in typescript

--- 02basics/myFunctions.ts
+++ 02basics/myFunctions.ts
@@ -1,6 +1,6 @@
-function addTwo(num: number) {
-  //   return num + 2;
-  return "hello";
+function addTwo(num: number): number {
+  return num + 2;
+  //   return "hello";
 }
 
 function getUpper(val: string) {
@@ -15,5 +15,33 @@ let myValue = addTwo(5);
 getUpper("bablu");
 signUpUser("bable", "mail@gmail.com", false);
 loginUser("bablu", "mail.com");
+
+// function getValue(myVal: number): boolean{
+//     if(myVal > 5)
+//     {
+//         return true;
+//     }
+
+//     return "200 ok"
+// }
+
+const getHello = (str: string): string => {
+  return "";
+};
+
+// const heros = ["thor", "spiderman", "ironman"];
+const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errMessage: string): void {
+  console.log(errMessage);
+}
+
+function handleError(errMessage: string): never {
+  throw new Error(errMessage);
+}
 
 export {};
