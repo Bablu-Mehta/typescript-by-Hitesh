22f11abeb6f9044dc394087c4f4888d89997d2ea
Bablu Mehta <babloomehta895012@gmail.com>
2024-03-10 11:45:07 +0530

    Array in Typescript

---
+++ 02basics/myArray.ts
@@ -0,0 +1,20 @@
+const superHeros: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [
+  [255, 255, 255],
+  [225, 225, 225],
+];
+
+superHeros.push("spiderman");
+heroPower.push(23);
+
+allUsers.push({ name: "bablu", isActive: true });
