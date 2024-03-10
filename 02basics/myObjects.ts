b54dd741231f11d4c01760dc4194462c47e4bae9
Bablu Mehta <babloomehta895012@gmail.com>
2024-03-10 10:27:26 +0530

    Type Aliases in Typescript

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,17 +1,29 @@
-const user = {
-  name: "bablu",
-  email: "bablue@dev.com",
-  isActive: true,
+// const user = {
+//   name: "bablu",
+//   email: "bablue@dev.com",
+//   isActive: true,
+// };
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+// let newUser = { name: "bablu", isPaid: false, email: "b@l.com" };
+
+// createUser(newUser);
+
+// function createCourse(): { name: string; price: number } {
+//   return { name: "reactJS", price: 399 };
+// }
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
 };
 
-function createUser({ name: string, isPaid: boolean }) {}
-
-let newUser = { name: "bablu", isPaid: false, email: "b@l.com" };
-
-createUser(newUser);
-
-function createCourse(): { name: string; price: number } {
-  return { name: "reactJS", price: 399 };
+function createUser(user: User): User {
+  return { name: "", email: "", isActive: true };
 }
+
+createUser({ name: "bablu", email: "email.com", isActive: false });
 
 export {};
