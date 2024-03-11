718a67fe0b815e4252edc277e7b446d146111a38
Bablu Mehta <babloomehta895012@gmail.com>
2024-03-11 11:58:35 +0530

    Enums in Typescript

---
+++ 02basics/myEnums.ts
@@ -0,0 +1,25 @@
+// enum seatChoice {
+//   AISLE = 10,
+//   MIDDLE,
+//   WINDOW,
+// }
+
+// enum seatChoice {
+//   AISLE = "aisle",
+//   MIDDLE = "middle",
+//   WINDOW = 'window',
+// }
+
+// enum seatChoice {
+//     AISLE = "aisle",
+//     MIDDLE = 4,
+//     WINDOW,
+//   }
+
+const enum seatChoice {
+  AISLE = "aisle",
+  MIDDLE = 4,
+  WINDOW,
+}
+
+const seat = seatChoice.AISLE;
