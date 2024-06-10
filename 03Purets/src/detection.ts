d9b831ac0ff63846750bef3482934967c3eb43c2
Bablu Mehta <babloomehta895012@gmail.com>
2024-06-10 08:32:40 +0530

    InstanceOf and Type Predicates

--- 03Purets/src/detection.ts
+++ 03Purets/src/detection.ts
@@ -29,3 +29,31 @@ function isAdminAccount(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = {
+  swim: () => void;
+};
+
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet
+    return "Fish Food";
+  } else {
+    pet
+    return "Bird Food";
+  }
+}
